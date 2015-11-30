49b7ddcfe3ba33974bdaa18ff27d27dfedbfb051
Christophe Benz <christophe.benz@gmail.com>
2015-11-30 11:47:43 +0100

    Add persist state

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
 import {compose, createStore} from "redux"
+import {persistState} from "redux-devtools"
 
 import DevTools from "./containers/DevTools"
 import rootReducer from "./reducers"
@@ -6,6 +7,8 @@ import rootReducer from "./reducers"
 
 const finalCreateStore = compose(
   DevTools.instrument(),
+  // Lets you write ?debug_session=<name> in address bar to persist debug sessions
+  persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/)),
 )(createStore)
 
 

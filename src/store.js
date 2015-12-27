03cb602ae7aac08606620c4fa6f38a6433e06e89
Christophe Benz <christophe.benz@gmail.com>
2015-12-27 17:23:34 +0100

    Be more explicit, like in redux-devtools doc

--- src/store.js
+++ src/store.js
@@ -8,8 +8,16 @@ import DevTools from "./containers/DevTools"
 const finalCreateStore = compose(
   DevTools.instrument(),
   // Lets you write ?debug_session=<name> in address bar to persist debug sessions
-  persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/)),
+  persistState(getDebugSessionKey()),
 )(createStore)
+
+
+function getDebugSessionKey() {
+  // You can write custom logic here!
+  // By default we try to read the key from ?debug_session=<key> in the address bar
+  const matches = window.location.href.match(/[?&]debug_session=([^&]+)\b/)
+  return matches && matches.length > 0 ? matches[1] : null
+}
 
 
 export default function configureStore(initialState) {

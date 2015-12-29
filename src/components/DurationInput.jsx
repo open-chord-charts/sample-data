efe902041ad4ec73cfb1fef67539b966706a8f13
Christophe Benz <christophe.benz@gmail.com>
2015-12-29 18:06:15 +0100

    Add width to duration input

--- src/components/DurationInput.jsx
+++ src/components/DurationInput.jsx
@@ -2,6 +2,9 @@ const DurationInput = ({onChange, value}) => (
   <input
     min={1}
     onChange={(event) => { onChange(event.target.valueAsNumber) }}
+    style={{
+      width: "3em",
+    }}
     type="number"
     value={value}
   />

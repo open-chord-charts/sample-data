feb94357114e01589ff08d1d214bb9679a04ba42
Christophe Benz <christophe.benz@gmail.com>
2016-05-21 18:58:40 +0200

    Add npm stats visualization

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,10 +1,12 @@
+import Visualizer from 'webpack-visualizer-plugin'
 import webpack from 'webpack'
 
 export default {
   plugins: [
     new webpack.ProvidePlugin({
       React: 'react' // For babel JSX transformation which generates React.createElement.
-    })
+    }),
+    new Visualizer()
   ],
   resolve: {
     extensions: ['', '.js', '.jsx']

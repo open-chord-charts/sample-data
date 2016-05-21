71766f353a4d3efd2208dd8212c9064c350a05ed
Christophe Benz <christophe.benz@gmail.com>
2016-05-21 18:58:51 +0200

    Enhance prod build

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -6,6 +6,9 @@ export default {
     new webpack.ProvidePlugin({
       React: 'react' // For babel JSX transformation which generates React.createElement.
     }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+    }),
     new Visualizer()
   ],
   resolve: {

615e1e32176d701893a21896503afe22953d92d3
Christophe Benz <christophe.benz@gmail.com>
2016-05-22 22:17:50 +0200

    Follow standard codestyle

--- src/constants.js
+++ src/constants.js
@@ -1,13 +1,10 @@
 // Music theory constants.
 
-
 export const QUALIFIERS = ['M', 'm', '7', 'm7b5']
 export const CHROMATIC_NOTES = ['Ab', 'A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G']
 export const DIATONIC_NOTES = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
 
-
 // Components rendering constants.
-
 
 export const NB_BARS_BY_ROW = 8
 export const CHART_PART_NAME_COLUMN_WIDTH = 30

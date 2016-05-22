16d1d2ef86f505ac13e23c6109f12568c69069e1
Christophe Benz <christophe.benz@gmail.com>
2016-05-22 22:28:47 +0200

    Fix component with r-dom

--- src/components/OneChord.js
+++ src/components/OneChord.js
@@ -10,26 +10,20 @@ export default class OneChord extends Component {
       r(
         'text',
         { dy: quality === 'm7b5' ? '1.2em' : '1em', style: { textAnchor: 'middle' }, x: '50%', y: '50%' },
-        r(
-          'tspan',
-          { style: { fontSize: height * 0.8 } },
-          rootNote
-        ),
-        r(
-          'tspan',
-          {
-            isRendered: quality !== 'M',
-            style: { fontSize: height * 0.5 }
-          },
-          quality === 'm7b5' ? 'm7' : quality
-        )
+        [
+          r(
+            'tspan',
+            { style: { fontSize: height * 0.8 } },
+            rootNote
+          ),
+          quality !== 'M' && r('tspan', { style: { fontSize: height * 0.5 } }, quality === 'm7b5' ? 'm7' : quality)
+        ]
       ),
-      r(
+      quality === 'm7b5' && r(
         'text',
         {
           dx: '-0.3em',
           dy: '1em',
-          isRendered: quality === 'm7b5',
           style: { fontSize: height * 0.4, textAnchor: 'end' },
           x: '100%',
           y: '0%'
